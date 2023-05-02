3d254563dbfb273282926d26cbd44fe9e4168130
forrest <aleremirror@gmail.com>
2023-05-02 21:35:19 +0800

    add: context servlet and web change

--- src/js/src/App.js
+++ src/js/src/App.js
@@ -1,11 +1,19 @@
 import { Component } from 'react';
 import './App.css';
 import { getAllStudents } from './client';
+import { Table, Avatar, Spin, } from 'antd';
+import { LoadingOutlined, } from '@ant-design/icons';
+import Container from './Container';
+
+
+const getIndicatorIcon = () => <LoadingOutlined />
+
 
 class App extends Component {
 
   state = {
-    students: []
+    students: [],
+    isFetch: false
   }
 
   componentDidMount() {
@@ -13,26 +21,55 @@ class App extends Component {
   }
 
   fetchStudents = () => {
+    this.setState({
+      isFetch: true
+    });
     getAllStudents()
       .then(res => res.json()
         .then(students => {
           console.log('------' + JSON.stringify(students));
-          this.setState({ students })
+          this.setState({
+            students,
+            isFetch: false
+          })
         }));
   }
 
   render() {
-    const { students } = this.state;
+    const { students, isFetch } = this.state;
+
+    if (isFetch) {
+      return <Container>
+        <Spin indicator={getIndicatorIcon()} />
+      </Container>
+    }
+
     if (students && students.length) {
-      return students.map((student, index) => {
-        return <div id={index}>
-          <h2>{student.studentId}</h2>
-          <h2>{student.firstName}</h2>
-          <h2>{student.lastName}</h2>
-          <h2>{student.email}</h2>
-          <h2>{student.gender}</h2>
-        </div >
-      });
+      const columns = [
+        {
+          title: "", key: "avatar", render: (text, student) =>
+            <Avatar size='large'>
+              {`${student.firstName.charAt(0).toUpperCase()}${student.lastName.charAt(0).toUpperCase()}`}
+            </Avatar>
+        },
+        { title: "StudentId", dataIndex: "studentId", key: "studentId", },
+        { title: "FirstName", dataIndex: "firstName", key: "firstName", },
+        { title: "LastName", dataIndex: "lastName", key: "lastName", },
+        { title: "Email", dataIndex: "email", key: "email", },
+        { title: "Gender", dataIndex: "gender", key: "gender", },
+      ];
+
+      return <Container><Table dataSource={students} columns={columns} pagination={false} rowKey='studentId' /></Container>;
+
+      // return students.map((student, index) => {
+      //   return <div id={index}>
+      //     <h2>{student.studentId}</h2>
+      //     <h2>{student.firstName}</h2>
+      //     <h2>{student.lastName}</h2>
+      //     <h2>{student.email}</h2>
+      //     <h2>{student.gender}</h2>
+      //   </div >
+      // });
     }
     return (
       <div> No Student </div >
